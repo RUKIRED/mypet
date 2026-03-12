05006ef0eecea67c5e1f556d17a79d1c37f58021
user <user@localhost>
2026-03-12 11:37:10 +0900

    add entity

--- myPetApp/src/main/java/sample/common/Repository/entity/Pet.java
+++ myPetApp/src/main/java/sample/common/Repository/entity/Pet.java
@@ -3,16 +3,14 @@ package sample.common.Repository.entity;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class AuthEntity {
-	@Id
+public class Pet {
+	private Long petId;
+	private String petNameString;
 	private UUID userId;
-	private String name;
-	private String password;
 }
