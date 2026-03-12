8be9ff686fc3f2e4c403aec5cb2d82119a54926b
user <user@localhost>
2026-03-12 11:42:46 +0900

    t

--- myPetApp/src/main/java/sample/common/Repository/entity/PetEntity.java
+++ myPetApp/src/main/java/sample/common/Repository/entity/PetEntity.java
@@ -3,13 +3,15 @@ package sample.common.Repository.entity;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Pet {
+public class PetEntity {
+	@Id
 	private Long petId;
 	private String petNameString;
 	private UUID userId;
